70ec0204d60586dc86eb31a96ea250c44d2f27c5
Maksim Shubny <maksim.shubny@yandex.ru>
2024-09-06 04:50:28 +0500

    add: adaptive

--- js/main.js
+++ js/main.js
@@ -1,35 +1,34 @@
 const swiper = new Swiper(".models-swiper", {
   speed: 400,
-  width: 1110,
   spaceBetween: 29,
   slidesPerView: 4,
-  loop: true,
+
   navigation: {
     nextEl: ".slider-button-next",
     prevEl: ".slider-button-prev",
   },
-  // breakpoints: {
-  //   // when window width is >= 320px
-  //   576: {
-  //     slidesPerView: 2,
-  //   },
-  //   // when window width is >= 480px
-  //   769: {
-  //     slidesPerView: 3,
-  //   },
-  //   // when window width is >= 992px
-  //   992: {
-  //     slidesPerView: 4,
-  //   },
-  //   // when window width is >= 1200px
-  //   1000: {
-  //     slidesPerView: 4,
-  //   },
-  //   // when window width is >= 1200px
-  //   1201: {
-  //     slidesPerView: 5,
-  //   },
-  // },
+  breakpoints: {
+    // when window width is >= 320px
+    576: {
+      slidesPerView: 0,
+    },
+    // when window width is >= 480px
+    769: {
+      slidesPerView: 1,
+    },
+    // when window width is >= 992px
+    992: {
+      slidesPerView: 2,
+    },
+    // when window width is >= 1200px
+    1000: {
+      slidesPerView: 3,
+    },
+    // when window width is >= 1200px
+    1201: {
+      slidesPerView: 4,
+    },
+  },
 });
 
 /* Создаем префикс +7, даже если вводят 8 или 9 */
